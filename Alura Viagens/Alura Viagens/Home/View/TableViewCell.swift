66bf87822b7458e4a02d340e72209495ec7a1c4e
Marco Aurélio <marco.goncalves@ab-inbev.com>
2019-11-05 10:16:58 -0300

    Add project

---
+++ Alura Viagens/Alura Viagens/Home/View/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  Alura Viagens
+//
+//  Created by Marco Aurelio on 04/11/19.
+//  Copyright © 2019 coda. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    @IBOutlet weak var imagemViagem: UIImageView!
+    
+}
---
+++ Alura Viagens/Alura Viagens/Model/Viagem.swift
@@ -0,0 +1,23 @@
+//
+//  Viagem.swift
+//  Alura Viagens
+//
+//  Created by Marco Aurelio on 31/10/19.
+//  Copyright © 2019 coda. All rights reserved.
+//
+
+import UIKit
+
+class Viagem: NSObject {
+    let titulo: String
+    let quantidadeDeDias: Int
+    let preco: String
+    let caminhoDaImagem: String
+    
+    init(titulo: String, quantidadeDeDias: Int, preco: String, caminhoDaImagem: String) {
+        self.titulo = titulo
+        self.quantidadeDeDias = quantidadeDeDias
+        self.preco = preco
+        self.caminhoDaImagem = caminhoDaImagem
+    }
+}
