e471ccabd0acde996bbafe6b936ed8f3d424f66d
Marco Aurélio <marco.goncalves@ab-inbev.com>
2019-11-05 14:44:30 -0300

    Implementando UICollectionView

---
+++ Alura Viagens/Alura Viagens/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  Alura Viagens
+//
+//  Created by Marco Aurelio on 05/11/19.
+//  Copyright © 2019 coda. All rights reserved.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imagemViagem: UIImageView!
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+}
